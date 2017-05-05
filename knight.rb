131bea4f2906006374643bb1f83aa7b9bee8a8e6
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-05-05 19:20:38 -0400

    added tree generator and bfs implementation

--- knight.rb
+++ knight.rb
@@ -2,70 +2,59 @@ require_relative '00_tree_node.rb'
 require 'byebug'
 
 class KnightPathFinder
-  attr_accessor :start_position
+  attr_accessor :start_position, :root_node
   def initialize(start_position)
     @start_position = start_position
-    @tree = PolyTreeNode.new(start_position)
-    @previously_visited_spaces = []
+    @visited_positions = [start_position]
+    @root_node = nil
     build_move_tree
   end
 
-  def moves(pos = start_position)
+  def new_move_positions(pos)
+    valid_surrounding_positions = KnightPathFinder.valid_moves(pos).reject do |e|
+      @visited_positions.include?(e)
+     end
+    @visited_positions += valid_surrounding_positions
+    return valid_surrounding_positions
+  end
+
+  def self.valid_moves(position)
     deltas = [[-2, -1], [-2, +1], [+2, -1], [+2, +1], [-1, -2], [-1, +2], [+1, -2], [+1, +2]]
-    x, y = pos
+    x, y = position
     pos_moves = deltas.map { |a, b| [x + a, b + y] }
     return pos_moves.select do |a,b|
       a < 0 || b < 0 || a >= 7 || b >= 7 ? false : true
     end
   end
 
-  def next_level
-    moves.each do |move|
-      node = PolyTreeNode.new(move)
-      node.parent = @tree
-      @tree.add_child(node)
-    end
-  end
-
-  def create_trees(position)
-    moves.next_level
-  end
-
-  def find_path(position)
-    round = 0
-    # while round < 8
-    while true
-      @tree.each do |node|
-        moves(node.value).map do |pos|
-          child = PolyTreeNode.new(pos)
-          child.parent = node
-          return find_parent(child) if child.value == position
-          @tree << child
-        end
-      end
-      # round += 1
-    end
+  def find_path(end_position)
+    trace_back_path(@root_node.bfs(end_position))
   end
 
 
-
-  def find_parent(node)
+  def trace_back_path(node)
     path = []
     until node.parent == nil
       path.unshift(node.value)
       node = node.parent
     end
-    path.unshift(@start_position)
+    path
   end
 
-  def search(target)
-    @tree.first.bfs(target)
-  end
 
   def build_move_tree(position = [0,0])
     #builds a move tree to be stored in an instance variable
-    moves(position).reject do |move|
-      @previously_visited_spaces.include?(move)
+    @root_node = PolyTreeNode.new(position)
+    queue = [@root_node]
+    until queue.empty?
+      parent = queue.shift
+      parent_position = parent.value
+      unvisited_surrounding_positions = new_move_positions(parent_position)
+      unvisited_surrounding_positions.each do |pos|
+        child_node = PolyTreeNode.new(pos)
+        child_node.parent = parent
+        queue << child_node
+      end
     end
   end
 
